4af9a0e3155f614ddaee0d0c65467a40bdb99b7f
campurro <campurro@yahoo.es>
2019-04-12 11:34:22 +0200

    Set hard fork height ( =~ April 26th)

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -84,7 +84,7 @@ const size_t   FUSION_TX_MIN_INPUT_COUNT                     = 12;
 const size_t   FUSION_TX_MIN_IN_OUT_COUNT_RATIO              = 4;
 
 const uint32_t UPGRADE_HEIGHT_V2                             = 0;
-const uint32_t UPGRADE_HEIGHT_V3                             = static_cast<uint32_t>(-1);
+const uint32_t UPGRADE_HEIGHT_V3                             = 2507230;
 const uint32_t UPGRADE_HEIGHT_V4                             = static_cast<uint32_t>(-1);
 const unsigned UPGRADE_VOTING_THRESHOLD                      = 90;               // percent
 const uint32_t UPGRADE_VOTING_WINDOW                         = EXPECTED_NUMBER_OF_BLOCKS_PER_DAY;  // blocks
